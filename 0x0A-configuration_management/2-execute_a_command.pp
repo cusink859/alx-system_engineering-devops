aab7003d259a63bf8acd3316394727541d78f4cd
cusink859 <gagyapong@servaco.com.gh>
2024-04-23 04:16:09 -0700

    update

--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,5 +1,5 @@
-# Execute a command
-exec { 'pkill killmenow':
-	path => '/usr/bin:/usr/sbin:/bin'
+# execute pkill command to kill bashscript file killmenow
+exec { 'pkill':
+  command => 'pkill -9 -f killmenow',
+  path    => ['/usr/bin', '/usr/sbin', '/bin']
 }
-
